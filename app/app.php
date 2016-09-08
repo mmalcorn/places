d54de0077939b30374e254ce97c486271b429792
alexandre leibler and meredith alcorn <alexandre@>
2016-09-08 16:48:15 -0400

    add session to initialize the page and create a place to save information

--- app/app.php
+++ app/app.php
@@ -2,6 +2,12 @@
     require_once(__DIR__ . "/../vendor/autoload.php");
     require_once(__DIR__ . "/../src/Place.php");
 
+    session_start();
+    if (empty($_SESSION["list_of_places"])){
+        $_SESSION["list_of_places"] = array();
+    }
+
+
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__ . '/../views'));
 
